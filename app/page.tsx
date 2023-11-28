48cebcf262448ec7f0edce199fe1b560c620ca7e
Martin Alejandro Mammana <martin.ale590@gmail.com>
2023-11-28 15:48:48 -0300

    Added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,41 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="">
-      <h1 className="">Dropbox Clone</h1>
+      <div className="flex flex-col lg:flex-row items-center bg-[#2b2929] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2b2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox. <br />
+            <br />
+            Storing everything for you and your business needs. All in one place
+          </h1>
+
+          <p className="pb-20">
+            Enhance your personal storage with Dropbox, offering a simple and
+            efficient way to upload, organize and access files from anywhere.
+            Securely store important documents and media, and experience the
+            convenience of easy file management and sharing in one centralized
+            solution
+          </p>
+
+          <Link
+            href="/dashboard"
+            className="flex cursor-pointer bg-blue-500 p-5 w-fit"
+          >
+            Try it for free!
+            <ArrowRight className="ml-10" />
+          </Link>
+        </div>
+
+        <div className="bg-[#1e1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg">
+            <source src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4" />
+            Your browser doesnot support the video tag
+          </video>
+        </div>
+      </div>
     </main>
   );
 }
